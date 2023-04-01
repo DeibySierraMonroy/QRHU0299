ae0cf2febdb16259489d94334a32ac063bd58a4d
deibydev <desierra@activos.com.co>
2023-03-31 20:23:54 -0500

    funcionalidad PL_OBTENER_CONTRATOS OK

--- QRHU0299.sql
+++ QRHU0299.sql
@@ -1,128 +1,227 @@
-/
-create or replace PACKAGE     RHU.QB_APLICATION_JRHU0055 AS
-
---**********************************************************************************************************
+create or replace PACKAGE RHU.QB_APLICATION_JRHU0055 AS
+ --**********************************************************************************************************
 --** NOMBRE SCRIPT        : QRHU0299
 --** OBJETIVO             : ADMINISTRACION DE GESTOR DE INCAPACIDADES
 --** ESQUEMA              : RHU
 --** AUTOR                : DEIBY SIERRA
 --** FECHA                : 20/02/2023
 --**********************************************************************************************************
-
 TYPE REFCURSOR IS REF CURSOR;
 
-PROCEDURE PL_OBTENER_TODAS_INCAPACIDADES(RCINC              OUT REFCURSOR,
-                                            VCESTADO_PROCESO   OUT VARCHAR2,
-                                            VCMENSAJE_PROCESO  OUT VARCHAR2);
+PROCEDURE PL_OBTENER_TODAS_INCAPACIDADES(
+    RCINC OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+);
 
-PROCEDURE PL_OBTENER_INC_DEV(RCINCDEV              OUT REFCURSOR,
-                                            VCESTADO_PROCESO   OUT VARCHAR2,
-                                            VCMENSAJE_PROCESO  OUT VARCHAR2);
+PROCEDURE PL_OBTENER_INC_DEV(
+    RCINCDEV OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+);
 
+PROCEDURE PL_OBTENER_TIP_INCAPACIDAD(
+    RTIPINCAPACIDAD OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+);
 
-PROCEDURE PL_OBTENER_TIP_INCAPACIDAD(       RTIPINCAPACIDAD    OUT REFCURSOR,
-                                            VCESTADO_PROCESO   OUT VARCHAR2,
-                                            VCMENSAJE_PROCESO  OUT VARCHAR2);
-                                            
-                                                                                        
-PROCEDURE PL_OBTENER_SUBTIP_INCAPACIDAD(    NTIPOINC           OUT NUMBER,
-                                            RSUBINCAPACIDAD    OUT REFCURSOR,
-                                            VCESTADO_PROCESO   OUT VARCHAR2,
-                                            VCMENSAJE_PROCESO  OUT VARCHAR2);                                            
+PROCEDURE PL_OBTENER_SUBTIP_INCAPACIDAD(
+    NTIPOINC OUT NUMBER,
+    RSUBINCAPACIDAD OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+);
+
+PROCEDURE PL_OBTENER_CONTRATOS(
+    VTDC_TD_EPL OUT VARCHAR2,
+    NEPL_ND OUT NUMBER,
+    NEMP_ND OUT NUMBER,
+    TDC_TD OUT VARCHAR2,
+    NTIPOINC OUT NUMBER,
+    RSUBINCAPACIDAD OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+);
 
 END QB_APLICATION_JRHU0055;
+
 /
-create or replace PACKAGE BODY     RHU.QB_APLICATION_JRHU0055 AS
---**********************************************************************************************************
+ create or replace PACKAGE BODY RHU.QB_APLICATION_JRHU0055 AS 
+ --**********************************************************************************************************
 --** NOMBRE SCRIPT        : QRHU0299
 --** OBJETIVO             : CONTIENE LA ADMINISTRACION DE GESTOR DE INCAPACIDADES
 --** ESQUEMA              : RHU
 --** AUTOR                : DEIBY SIERRA
 --** FECHA                : 20/02/2023
 --**********************************************************************************************************
-PROCEDURE PL_OBTENER_TODAS_INCAPACIDADES(RCINC              OUT REFCURSOR,
-                                            VCESTADO_PROCESO   OUT VARCHAR2,
-                                            VCMENSAJE_PROCESO  OUT VARCHAR2)IS
-                         
-    BEGIN
+PROCEDURE PL_OBTENER_TODAS_INCAPACIDADES(
+    RCINC OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+) IS 
+BEGIN 
+OPEN RCINC FOR
+SELECT
+    *
+FROM
+    INCAPACIDAD
+WHERE
+    ROWNUM <= 100;
 
-        OPEN RCINC FOR
-            SELECT * FROM INCAPACIDAD WHERE ROWNUM <= 100;
+VCESTADO_PROCESO := 'S';
 
-        VCESTADO_PROCESO := 'S';
-        VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
+VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
 
-    EXCEPTION
-        WHEN OTHERS THEN
-            VCESTADO_PROCESO := 'N';
-            VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_TODAS_INCAPACIDADES, causada por: 
-            '||SQLERRM||' -- Linea: ' || dbms_utility.format_error_backtrace();
+EXCEPTION
+WHEN OTHERS THEN VCESTADO_PROCESO := 'N';
+
+VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_TODAS_INCAPACIDADES, causada por: 
+            ' || SQLERRM || ' -- Linea: ' || dbms_utility.format_error_backtrace();
 
 END PL_OBTENER_TODAS_INCAPACIDADES;
 
-PROCEDURE PL_OBTENER_INC_DEV(RCINCDEV              OUT REFCURSOR,
-                                            VCESTADO_PROCESO   OUT VARCHAR2,
-                                            VCMENSAJE_PROCESO  OUT VARCHAR2)IS
-                         
-    BEGIN
+PROCEDURE PL_OBTENER_INC_DEV(
+    RCINCDEV OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+) IS 
+BEGIN 
+OPEN RCINCDEV FOR
+SELECT
+    *
+FROM
+    INCAPACIDAD
+WHERE
+    INC_ESTADO = 'DEV'
+    AND ROWNUM <= 100;
 
-        OPEN RCINCDEV FOR
-            SELECT * FROM INCAPACIDAD WHERE INC_ESTADO = 'DEV' AND ROWNUM <= 100;
+VCESTADO_PROCESO := 'S';
 
-        VCESTADO_PROCESO := 'S';
-        VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
+VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
 
-    EXCEPTION
-        WHEN OTHERS THEN
-            VCESTADO_PROCESO := 'N';
-            VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_INC_DEV, causada por: 
-            '||SQLERRM||' -- Linea: ' || dbms_utility.format_error_backtrace();
+EXCEPTION
+WHEN OTHERS THEN VCESTADO_PROCESO := 'N';
+
+VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_INC_DEV, causada por: 
+            ' || SQLERRM || ' -- Linea: ' || dbms_utility.format_error_backtrace();
 
 END PL_OBTENER_INC_DEV;
 
-PROCEDURE PL_OBTENER_TIP_INCAPACIDAD(       RTIPINCAPACIDAD    OUT REFCURSOR,
-                                            VCESTADO_PROCESO   OUT VARCHAR2,
-                                            VCMENSAJE_PROCESO  OUT VARCHAR2)IS                  
-    BEGIN
-        OPEN RTIPINCAPACIDAD FOR
-            SELECT * FROM RHU.TIPO_INCAPACIDAD 
-            WHERE TIPOINC_ESTADO = 'ACTIVO';
+PROCEDURE PL_OBTENER_TIP_INCAPACIDAD(
+    RTIPINCAPACIDAD OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+) IS 
+BEGIN 
+OPEN RTIPINCAPACIDAD FOR
+SELECT
+    *
+FROM
+    RHU.TIPO_INCAPACIDAD
+WHERE
+    TIPOINC_ESTADO = 'ACTIVO';
 
-        VCESTADO_PROCESO := 'S';
-        VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
-    EXCEPTION
-        WHEN OTHERS THEN
-            VCESTADO_PROCESO := 'N';
-            VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_TIP_INCAPACIDAD, causada por: 
-            '||SQLERRM||' -- Linea: ' || dbms_utility.format_error_backtrace();
+VCESTADO_PROCESO := 'S';
+
+VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
+
+EXCEPTION
+WHEN OTHERS THEN VCESTADO_PROCESO := 'N';
+
+VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_TIP_INCAPACIDAD, causada por: 
+            ' || SQLERRM || ' -- Linea: ' || dbms_utility.format_error_backtrace();
 
 END PL_OBTENER_TIP_INCAPACIDAD;
 
+PROCEDURE PL_OBTENER_SUBTIP_INCAPACIDAD(
+    NTIPOINC OUT NUMBER,
+    RSUBINCAPACIDAD OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+) IS 
+BEGIN 
+OPEN RSUBINCAPACIDAD FOR
+SELECT
+    *
+FROM
+    RHU.SUBTIPO_INCAPACIDAD
+WHERE
+    SUBTIPOINC_ESTADO = 'ACTIVO'
+    AND TIPOINC_CODIGO = NTIPOINC;
 
-PROCEDURE PL_OBTENER_SUBTIP_INCAPACIDAD(    NTIPOINC           OUT NUMBER,
-                                            RSUBINCAPACIDAD    OUT REFCURSOR,
-                                            VCESTADO_PROCESO   OUT VARCHAR2,
-                                            VCMENSAJE_PROCESO  OUT VARCHAR2)IS                      
-    BEGIN
-        OPEN RSUBINCAPACIDAD FOR
-            SELECT * FROM RHU.SUBTIPO_INCAPACIDAD 
-            WHERE SUBTIPOINC_ESTADO = 'ACTIVO' 
-            AND TIPOINC_CODIGO = NTIPOINC;
+VCESTADO_PROCESO := 'S';
 
-        VCESTADO_PROCESO := 'S';
-        VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
+VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
 
-    EXCEPTION
-        WHEN OTHERS THEN
-            VCESTADO_PROCESO := 'N';
-            VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_SUBTIP_INCAPACIDAD, causada por: 
-            '||SQLERRM||' -- Linea: ' || dbms_utility.format_error_backtrace();
+EXCEPTION
+WHEN OTHERS THEN VCESTADO_PROCESO := 'N';
+
+VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_SUBTIP_INCAPACIDAD, causada por: 
+            ' || SQLERRM || ' -- Linea: ' || dbms_utility.format_error_backtrace();
 
 END PL_OBTENER_SUBTIP_INCAPACIDAD;
 
+PROCEDURE PL_OBTENER_CONTRATOS(
+    VTDC_TD_EPL OUT VARCHAR2,
+    NEPL_ND OUT NUMBER,
+    NEMP_ND OUT NUMBER,
+    TDC_TD OUT VARCHAR2,
+    NTIPOINC OUT NUMBER,
+    RSUBINCAPACIDAD OUT REFCURSOR,
+    VCESTADO_PROCESO OUT VARCHAR2,
+    VCMENSAJE_PROCESO OUT VARCHAR2
+) IS 
+BEGIN 
+OPEN RSUBINCAPACIDAD FOR 
+WITH CONTRATOS_ACTIVOS AS (
+    SELECT
+        CTO.*,
+        FB_NOM_EMPRESA(TDC_TD_FIL, EMP_ND_FIL) NOMBRE_EMPRESA_USUARIA,
+        FB_NOM_EMPRESA(TDC_TD, EMP_ND) NOMBRE_EMPRESA_PRINCIPAL
+    FROM
+        CONTRATO CTO
+    WHERE
+        ECT_SIGLA = 'ACT'
+        AND TDC_TD_EPL = VTDC_TD_EPL
+        AND EPL_ND = NEPL_ND
+        AND TDC_TD = TDC_TD
+        AND EMP_ND = NEMP_ND
+),
+CONTRATOS_INACTIVOS AS (
+    SELECT
+        CTO.*,
+        FB_NOM_EMPRESA(TDC_TD_FIL, EMP_ND_FIL) NOMBRE_EMPRESA_USUARIA,
+        FB_NOM_EMPRESA(TDC_TD, EMP_ND) NOMBRE_EMPRESA_PRINCIPAL
+    FROM
+        CONTRATO CTO
+    WHERE
+        ECT_SIGLA IN ('INA', 'RET')
+        AND CTO_FECRET BETWEEN TO_DATE(SYSDATE -180, 'yyyy/mm/dd')
+        AND TO_DATE(SYSDATE, 'yyyy/mm/dd')
+        AND TDC_TD_EPL = VTDC_TD_EPL
+        AND EPL_ND = NEPL_ND
+        AND TDC_TD = TDC_TD
+        AND EMP_ND = NEMP_ND
+)
+SELECT
+    *
+FROM
+    CONTRATOS_INACTIVOS
+UNION
+SELECT
+    *
+FROM
+    CONTRATOS_ACTIVOS;
 
+VCESTADO_PROCESO := 'S';
+VCMENSAJE_PROCESO := 'Procedimiento ejecutado exitosamente';
 
+EXCEPTION
+WHEN OTHERS THEN VCESTADO_PROCESO := 'N';
+VCMENSAJE_PROCESO := 'ERROR no controlado en RHU.QB_APLICATION_JRHU0055.PL_OBTENER_CONTRATOS, causada por: 
+            ' || SQLERRM || ' -- Linea: ' || dbms_utility.format_error_backtrace();
 
-
+END PL_OBTENER_CONTRATOS;
 
 END QB_APLICATION_JRHU0055;
